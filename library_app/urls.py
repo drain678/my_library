a97a97a980f4a1ad5a64ba2e6e5352bee7ba47d3
siriusdevs <tenigin.aa@talantiuspeh.ru>
2024-04-10 14:38:39 +0300

    all basic views

--- library_app/urls.py
+++ library_app/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='homepage'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/', views.book_view, name='book'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/', views.author_view, name='author'),
+    path('genres/', views.GenreListView.as_view(), name='genres'),
+    path('genre/', views.genre_view, name='genre'),
 ]
--- library_app/views.py
+++ library_app/views.py
@@ -1,4 +1,7 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.core.paginator import Paginator
 from .models import Book, Author, Genre
 
 
@@ -12,3 +15,35 @@ def main(request):
             'genres': Genre.objects.count(),
         }
     )
+def create_listview(model_class, plural_name, template):
+    class CustomListView(ListView):
+        model = model_class
+        template_name = template
+        paginate_by = 10
+        context_object_name = plural_name
+
+        def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+            context = super().get_context_data(**kwargs)
+            instances = model_class.objects.all()
+            paginator = Paginator(instances, 10)
+            page = self.request.GET.get('page')
+            page_obj = paginator.get_page(page)
+            context[f'{plural_name}_list'] = page_obj
+            return context
+    return CustomListView
+BookListView = create_listview(Book, 'books', 'catalog/books.html')
+GenreListView = create_listview(Genre, 'genres', 'catalog/genres.html')
+AuthorListView = create_listview(Author, 'authors', 'catalog/authors.html')
+    
+def create_view(model, model_name, template):
+    def view(request):
+        target = model.objects.get(id=request.GET.get('id', ''))
+        return render(
+            request,
+            template,
+            context={model_name: target},
+        )
+    return view
+book_view = create_view(Book, 'book', 'entities/book.html')
+author_view = create_view(Author, 'author', 'entities/author.html')
+genre_view = create_view(Genre, 'genre', 'entities/genre.html')
